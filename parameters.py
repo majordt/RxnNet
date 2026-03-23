a67ea2ce5a59c7945f1abfe1db2581fe7cd7b5ee
jishnunsj <jishnunsj@gmail.com>
2026-03-23 17:38:30 +0200

    bug fix in qm module

--- parameters.py
+++ parameters.py
@@ -15,7 +15,7 @@ REACTION_LIST_NAME = 'carbocations'
 # REACTION_LIST_NAME = 'carbocations'
 
 OUT_FILE_FOR_RUN = f"{MOLECULE_NAME}.log"
-MAX_PATH_LENGTH_TO_RUN = 2
+MAX_PATH_LENGTH_TO_RUN = 5
 ELIMINATE = ['=C=', '=[N+]=', '[CH2+]']
 MAX_TFD_VALUE = 1 # optional - maximum value is 1
 IMPOSSIBLE_SMILES = ['C[C@]12CCCCC[C@@H]1C2', 'C[C@@]12CCCCC[C@H]1C2']
@@ -28,43 +28,44 @@ SMILE_TO_ANALYZE = 'CC1=CC[C@@](C)([C@@]2(C)CCC[C+]2C)CC1'   #trichodiene
 # SMILE_TO_ANALYZE = 'C/C1=C\CC[C+](C)[C@H]2C[C@@]3(C)CC[C@@H](C(C)C)[C@@H]3C[C@@H]2/C(C)=C/CC1'  #K3
 # SMILE_TO_ANALYZE = 'C/C1=C\C[C@@]2(C)CC[C@@H](C(C)C)[C@@H]2C/C=C(\C)[C@@H]2CC[C+](C)[C@H]2CC1'  #K2
 
-MAX_PATH_LENGTH_TO_ANALYZE = 1
+MAX_PATH_LENGTH_TO_ANALYZE = 5
 OUT_FILE_FOR_ANALYZE = f"{MOLECULE_NAME}_analyze.log"
-TS_NUM_FRAMES = 16
+TS_NUM_FRAMES = 18
 ALIGN_TO_PRODUCT = False     # True for template-based approach
                              # False for distance-restraint-based approach
 NUM_CONFS_TO_GENERATE = 100  # Number of conformers initially generated
-CHARGE = 1
 ############################################################################################################
 
 # QM
 FOLDER_NAME = ''            # Add the folder name in which geodesic MEPs are generated
 OUT_FILE_FOR_QM = f"{MOLECULE_NAME}_QM.log"
+QM_SET_FOLDERS = 'set0'     #Optional: run QM only for these subfolders under Results/MOLECULE_NAME/FOLDER_NAME
+                            #If omitted or None, all folders with prefix "set" are used.
 CHARGE = 1
 MULTIPLICITY = 1
 ORCA_PATH = ''              # Path where ORCA is installed
 OMPI_PATH = ''              # OpenMPI root path
 XTB_PATH = ''               # XTB root path
-SETENV_ORCA = [f'LD_LIBRARY_PATH {OMPI_PATH}/lib:$LD_LIBRARY_PATH',
-               f'PATH {ORCA_PATH}:$PATH',
-               f'PATH {OMPI_PATH}/bin:$PATH',
-               f'XTBEXE {XTB_PATH}/bin/xtb']
+ORCA_PATH = f'{ORCA_PATH}/orca'
+OMPI_BIN_PATH = '{OMPI_PATH}/bin/'
+OMPI_LIB_PATH = '{OMPI_PATH}/lib/'
 
 SEMI_EMPIRICAL = True                  # True for performing SE NEB calculation
 SEMI_EMPIRICAL_METHOD = 'GFN2-xTB'
 SEMI_EMPIRICAL_KEYWORDS = []
 SEMI_EMPIRICAL_NPROCS = 16
-SE_INITIAL_GUESS = True                # True to use geodesic paths as initial guess for NEB
+SE_INITIAL_GUESS = True                # True using geodesic paths as initial guess for NEB
 SEMI_EMPIRICAL_MAXITER_GEOM = 200 
 SEMI_EMPIRICAL_MAXITER_SCF = 500 
 
 DFT = False                            # True for DFT NEB 
+DFT_INPUT_ONLY = True                  # True for only writing the DFT input files
+DFT_INITIAL_GUESS = True               # True to use XTB NEB MEP as initial guess
 DFT_METHOD = 'M062X'
 DFT_KEYWORDS = ['defgrid3', 'RIJCOSX', 'def2/J']
-DFT_NPROCS = 20
-DFT_INITIAL_GUESS = True               # True to use XTB NEB MEP as initial guess
-DFT_MAXITER_GEOM = 1000 
-DFT_MAXITER_SCF = 1000
-NEB_MAXITER = 1000 
+DFT_NPROCS = 16
+DFT_MAXITER_GEOM = 500 
+DFT_MAXITER_SCF = 500
+NEB_MAXITER = 500 
 
 
